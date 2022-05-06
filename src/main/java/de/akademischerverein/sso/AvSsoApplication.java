e44fb4717a4e15ef1bed3484ac6b3fc4b901632c
Jonathan Horn <jonathan@autoit4you.de>
2022-05-06 14:54:24 +0200

    Initial commit

---
+++ src/main/java/de/akademischerverein/sso/AvSsoApplication.java
@@ -0,0 +1,13 @@
+package de.akademischerverein.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvSsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/akademischerverein/sso/AvSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.akademischerverein.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
