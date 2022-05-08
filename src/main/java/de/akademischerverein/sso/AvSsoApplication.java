ce7dbfae4cf89c0dbf2e50dce02920d933d5785e
Jonathan Horn <jonathan@autoit4you.de>
2022-05-08 14:27:44 +0200

    Enable scheduling

--- src/main/java/de/akademischerverein/sso/AvSsoApplication.java
+++ src/main/java/de/akademischerverein/sso/AvSsoApplication.java
@@ -1,12 +1,11 @@
 package de.akademischerverein.sso;
 
-import de.akademischerverein.sso.auth.AvaService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AvSsoApplication {
 
     public static void main(String[] args) {
